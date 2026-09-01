f70a96977d75f824e5405e2f4ee5e1085586192f
yassinepatin@gmail.com <yassinedzaouha2@gmail.com>
2026-09-01 09:32:06 +0100

    challenge de créez un programme qui stocke dans des variables les informations, et afichier toutes les information

---
+++ day1/variables/challenge1.js
@@ -0,0 +1,11 @@
+let Pseudo = "ShadowX"
+let Niveau =12
+let Âge =21
+let Score =4580
+let Pays = "Maroc"
+
+console.log("Pseudo: " , Pseudo);
+console.log("Niveau: " , Niveau);
+console.log("Âge: " , Âge);
+console.log("Score: " , Score);
+console.log("Pays: " , Pays);
