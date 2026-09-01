eca8cad4f8238b0ee6fa80110e65d95c54b14a4a
yassinedzaouha <yassinepatin@gmail.com>
2026-09-01 22:51:17 +0100

    push the files : package-lock.json , package.json , .gitignore

---
+++ day1/variables/challenget1.js
@@ -0,0 +1,7 @@
+var prompt = require('prompt-sync')();
+var n = prompt ("donner moi le score partie 1;")
+var n = prompt ("donner moi le score partie 2;")
+var o = prompt ("donner moi le score partie 3;")
+var p = prompt ("donner moi le score partie 4;")
+var l = n + i + o + p;
+console.log('le score totale est ,'+ parseInt(l));
