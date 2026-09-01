601ba27099aa03bbfaa1d09110be8d80bfa0d207
yassinepatin@gmail.com <yassinedzaouha2@gmail.com>
2026-09-01 11:08:44 +0100

    challenge of calculate the total and the average of 4 scores

---
+++ day1/variables/challenge6.js
@@ -0,0 +1,9 @@
+let prompt = require("prompt-sync")();
+let score1 = Number(prompt("donner score partie 1: "))
+let score2 = Number(prompt("donner score partie 2: "))
+let score3 = Number(prompt("donner score partie 3: "))
+let score4 = Number(prompt("donner score partie 4: "))
+let scoreTotal = score1 + score2 + score3 + score4 ;
+let moyenne = scoreTotal / 4;
+console.log("score total : ", scoreTotal);
+console.log("moyenne : ", moyenne);
