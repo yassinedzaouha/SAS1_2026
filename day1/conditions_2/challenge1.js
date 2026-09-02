6d0bdf999259c7ad9979ef42c619a9d939ceaaf3
yassinedzaouha <yassinepatin@gmail.com>
2026-09-02 02:17:29 +0100

    challenge de tarif de livraison

--- day1/conditions_2/challenge1.js
+++ day1/conditions_2/challenge1.js
@@ -17,7 +17,3 @@ if (livraison) {
   console.log("livraison :", livraison);
   console.log("total a psyer : ", commandeM + livraison, "DH");
 }
-let i;
-for(i = 0;i <= 5 ; i++){
-    console.log(i)
-}
