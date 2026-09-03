964fa4c96de13cd119472d2a4880431571e6598e
yassinepatin@gmail.com <yassinedzaouha2@gmail.com>
2026-09-03 08:54:55 +0100

    challenge de vérifier si un nombre est premier

--- day2/boucles1/challenge13.js
+++ day2/boucles1/challenge13.js
@@ -2,18 +2,15 @@ const prompt = require("prompt-sync")()
 let N = Number(prompt("donner un nombre :"))
 
 let i;
+let bool = true
 for( i=2; i < N; i++){
-    if(N % i != 0){
-        continue;
-    }
-    else{
+    if(N % i == 0){
+        console.log(N, "n'est pas un nombre premier.")
+        bool = false
         break;
     }
 }
 
-if(i == N){
-    console.log(N, "est un nombre premier.")
-}
-else {
-    console.log(N, "n'est pas un nombre premier.")
+if(bool){
+    console.log(N, "est un nombre premier.");
 }
